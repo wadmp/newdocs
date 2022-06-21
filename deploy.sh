a022befebeee277c8fc60a288beab254640445b0
Jan Svoboda <jan.svoboda@advantech-bb.cz>
2022-06-21 15:13:38 +0200

    Updated deploy and release notes

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ npm run docs:build
 cd src/.vuepress/dist
 
 # if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+echo 'newdocs.wadmp.com' > CNAME
 
 git init
 git add -A
@@ -20,6 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:wadmp/newdocs.wadmp.github.io.git master:gh-pages
+# git push -f git@github.com:wadmp/newdocs.wadmp.github.io.git master:gh-pages
+git push -f git@github.com:wadmp/newdocs.wadmp.github.io.git master
 
 cd -
